aedc69e17f58a62b7f9d07e6344013c885b878f0
Ltz.dev <167724343+Lt892@users.noreply.github.com>
2024-07-15 13:55:56 -0400

    Actualizar guardar.php

--- guardar.php
+++ guardar.php
@@ -2,4 +2,9 @@
  $user = "root"; 
  $pasword = "";
  $servidor = "localhost";
- $base de datos = "folmulario
+ $base de datos = "formulario";
+
+$conexion =mysqli_conect ($servidor, $usuario,"") or die ("Error con el servidor de la base de datos");
+
+
+
